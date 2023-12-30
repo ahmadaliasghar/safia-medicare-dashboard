8fad7aeaae76dc2c4bc1ac6d22e101947cd63164
Ahmad Ali Asghar <ahmadali2882@yahoo.com>
2023-12-30 14:00:20 +0500

    Safia Medicare Dashboard

---
+++ utils/constant.js
@@ -0,0 +1,11 @@
+export const user = {
+    user: { 
+    name: 'Ahmad',
+    last_name: 'Ali',
+    email: 'ahmadaliasgher@gmail.com',
+    role: 1
+}
+}
+export const ROLE_ADMIN = 1
+export const ROLE_PROVIDER = 2
+export const ROLE_AGENT = 3
---
+++ utils/routes.js
@@ -0,0 +1,14 @@
+export const links = [
+    {
+        label: 'Dashboard',
+        path: '/',
+    },
+    {
+        label: 'Doctors',
+        path: '/doctors',
+    },
+    {
+        label: 'Services',
+        path: '/services',
+    },
+]
