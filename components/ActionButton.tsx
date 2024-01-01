4fd632b94c0ef8a2895dc865001e570ac8bafc57
Ahmad Ali Asghar <ahmadali2882@yahoo.com>
2024-01-01 15:52:47 +0500

    Added Appointmwnts

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { ReactNode, MouseEventHandler } from 'react';
 
-const ActionButton = ({ children, onClick, type }) => {
+type ActionType = 'success' | 'default';
+
+interface ActionButtonProps {
+  children: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  type?: ActionType;
+}
+
+const ActionButton: React.FC<ActionButtonProps> = ({ children, onClick, type = 'default' }) => {
   return (
-    <button onClick={onClick} className={`px-2 py-1 border rounded ${type === "success" ? 'bg-blue-400' : 'bg-red-600'} text-white`} >
+    <button
+      onClick={onClick}
+      className={`px-2 py-1 border rounded ${type === 'success' ? 'bg-blue-400' : 'bg-red-600'} text-white`}
+    >
       {children}
     </button>
   );
