ee4cd93b80a35162904a5ae23b3732ccd8f42021
Ahmad Ali Asghar <ahmadali2882@yahoo.com>
2024-02-03 23:28:03 +0500

    Added Patient Filter

--- components/Table/PatientTable.tsx
+++ components/Table/PatientTable.tsx
@@ -1,10 +1,13 @@
+'use client'
 import { Patient } from "@/types";
 import Image from "next/image";
 import Icon from "@/assets/images/Profile.jpg"
 import ActionButton from "../ActionButton";
 import Link from "next/link";
-
-
+import { useState } from "react";
+import * as Select from '@radix-ui/react-select';
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import '@/styles/Select.module.css'
 
 export const calculateAge = (dob: string | undefined): string => {
   if (!dob) return 'N/A';
@@ -33,6 +36,40 @@ interface PatientTableProps {
 }
 
 const PatientTable: React.FC<PatientTableProps> = ({ data }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterCriteria, setFilterCriteria] = useState<string | null>('name'); // Set default criteria to 'name'
+
+  const filteredData = data.filter((patient) => {
+    const fullName = `${patient.firstName} ${patient.lastName}`.toLowerCase();
+
+    if (
+      !searchTerm ||
+      fullName.includes(searchTerm.toLowerCase()) ||
+      patient.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      patient.contact.includes(searchTerm)
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  if (filterCriteria) {
+    filteredData.sort((a, b) => {
+      const criteriaA =
+        filterCriteria === 'name'
+          ? `${a.firstName} ${a.lastName}`
+          : a[filterCriteria as keyof Patient];
+
+      const criteriaB =
+        filterCriteria === 'name'
+          ? `${b.firstName} ${b.lastName}`
+          : b[filterCriteria as keyof Patient];
+
+      return criteriaA.localeCompare(criteriaB);
+    });
+  }
+
+
 
   return (
     <div className="rounded-sm border border-stroke bg-white shadow-default ">
@@ -46,6 +83,71 @@ const PatientTable: React.FC<PatientTableProps> = ({ data }) => {
           </ActionButton>
         </Link>
 
+      </div>
+      <div className="py-2 px-4 md:px-6 xl:px-7.5 flex items-center">
+        <input
+          type="text"
+          placeholder="Search by name, email, or contact"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-stroke p-2 w-full rounded-md focus:outline-none"
+        />
+      </div>
+
+      {/* Filter dropdown */}
+      <div className="py-2 px-4 md:px-6 xl:px-7.5 flex items-center">
+        <label className="text-black font-medium mr-2">Filter by:</label>
+        <Select.Root>
+    <Select.Trigger className="SelectTrigger" aria-label="Food">
+      <Select.Value placeholder="Select a fruit…" />
+      <Select.Icon className="SelectIcon">
+        <ChevronDownIcon />
+      </Select.Icon>
+    </Select.Trigger>
+    <Select.Portal>
+      <Select.Content className="SelectContent">
+        <Select.ScrollUpButton className="SelectScrollButton">
+          <ChevronUpIcon />
+        </Select.ScrollUpButton>
+        <Select.Viewport className="SelectViewport">
+          <Select.Group>
+            <Select.Label className="SelectLabel">Fruits</Select.Label>
+            <Select.SelectItem value="apple">Apple</Select.SelectItem>
+            <Select.SelectItem value="banana">Banana</Select.SelectItem>
+            <Select.SelectItem value="blueberry">Blueberry</Select.SelectItem>
+            <Select.SelectItem value="grapes">Grapes</Select.SelectItem>
+            <Select.SelectItem value="pineapple">Pineapple</Select.SelectItem>
+          </Select.Group>
+
+          <Select.Separator className="SelectSeparator" />
+
+          <Select.Group>
+            <Select.Label className="SelectLabel">Vegetables</Select.Label>
+            <Select.SelectItem value="aubergine">Aubergine</Select.SelectItem>
+            <Select.SelectItem value="broccoli">Broccoli</Select.SelectItem>
+            <Select.SelectItem value="carrot" disabled>
+              Carrot
+            </Select.SelectItem>
+            <Select.SelectItem value="courgette">Courgette</Select.SelectItem>
+            <Select.SelectItem value="leek">Leek</Select.SelectItem>
+          </Select.Group>
+
+          <Select.Separator className="SelectSeparator" />
+
+          <Select.Group>
+            <Select.Label className="SelectLabel">Meat</Select.Label>
+            <Select.SelectItem value="beef">Beef</Select.SelectItem>
+            <Select.SelectItem value="chicken">Chicken</Select.SelectItem>
+            <Select.SelectItem value="lamb">Lamb</Select.SelectItem>
+            <Select.SelectItem value="pork">Pork</Select.SelectItem>
+          </Select.Group>
+        </Select.Viewport>
+        <Select.ScrollDownButton className="SelectScrollButton">
+          <ChevronDownIcon />
+        </Select.ScrollDownButton>
+      </Select.Content>
+    </Select.Portal>
+  </Select.Root>
       </div>
       <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
         <div className="col-span-2 flex items-center">
@@ -65,7 +167,7 @@ const PatientTable: React.FC<PatientTableProps> = ({ data }) => {
         </div>
       </div>
 
-      {data?.map((patient, index) => (
+      {filteredData?.map((patient, index) => (
         <div
           className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
           key={index}
