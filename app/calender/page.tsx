916235c7b611a92f76d8477146fa57f00ead4cf7
MHaris03 <xhkharis2@gmail.com>
2024-02-10 16:09:05 +0500

    Add calender event

--- app/calender/page.tsx
+++ app/calender/page.tsx
@@ -1,30 +1,43 @@
 'use client'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-
-const events = [
-  { title: 'Meeting', start: new Date() }
-]
+import { useEffect, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { useGetAppointmentsQuery } from '@/features/appointmentSlice';
 
 export default function Page() {
+  const { data: appointmentsData } = useGetAppointmentsQuery();
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    if (appointmentsData && Array.isArray(appointmentsData.appointments)) {
+      const formattedAppointments = appointmentsData.appointments.map(appointment => ({
+        title: appointment.title,
+        start: `${appointment.date}T${appointment.time}`,
+      }));
+      setAppointments(formattedAppointments);
+    }
+  }, [appointmentsData]);
+
+
   return (
     <div className='m-4'>
       <FullCalendar
         plugins={[dayGridPlugin]}
         initialView='dayGridMonth'
         weekends={false}
-        events={events}
+        events={appointments}
         eventContent={renderEventContent}
       />
     </div>
-  )
+  );
 }
 
 function renderEventContent(eventInfo) {
   return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  )
+    <div className='bg-blue-500'>
+      <b className="text-white">{eventInfo.timeText}</b>
+      <i className="text-white">{eventInfo.event.title}</i>
+    </div>
+  );
 }
+
